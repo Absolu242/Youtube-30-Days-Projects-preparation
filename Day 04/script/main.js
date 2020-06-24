2bce151f41ccaf4a9ea30bad7fffc81390c0c0d1
Rahan Bouess <wangleibniz01@gmail.com>
2020-06-24 14:33:23 +0800

    project day 4

---
+++ Day 04/script/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $(".toggle").on("click", function(){
+        
+        if($(".menu").hasClass("active")){
+            $(".menu").removeClass("active");
+        }
+        else{
+            $(".menu").addClass("active");
+        }
+    })
+});
